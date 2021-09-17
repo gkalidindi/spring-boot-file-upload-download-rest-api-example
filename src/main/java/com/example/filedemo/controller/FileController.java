eb9a6a7bb032be6825dda0cae8e7f25975c697eb
gkalidindi <67965373+gkalidindi@users.noreply.github.com>
2021-09-18 00:32:33 +0530

    Update FileController.java
    Added Upload file using Json payload method

--- src/main/java/com/example/filedemo/controller/FileController.java
+++ src/main/java/com/example/filedemo/controller/FileController.java
@@ -14,9 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,7 +30,16 @@ public class FileController {
     private FileStorageService fileStorageService;
 
     @PostMapping("/uploadFile")
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file)  {
+        String encodedString = null;
+        try {
+            encodedString = Base64.getEncoder().encodeToString(file.getBytes());
+            System.out.println("Base64 encoded string: " + encodedString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Store File
         String fileName = fileStorageService.storeFile(file);
 
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -38,6 +49,41 @@ public class FileController {
 
         return new UploadFileResponse(fileName, fileDownloadUri,
                 file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadFileAsJson")
+    public void uploadFileAsJson(@RequestBody Map<String, Object> payload) throws IOException {
+        System.out.println("Request Payload: " + payload);
+        String fileName = (String) payload.get("fileName");
+        String purpose = (String) payload.get("purpose");
+        String title = (String) payload.get("title");
+        String linkExpiresTime = (String) payload.get("linkExpiresTime");
+
+        // Get Base64 encoded file content and decode it
+        String encodedString = (String) payload.get("file");
+        fileStorageService.storeFile(fileName, encodedString);
+
+        /*byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        File file = new File("/Users/gkalidindi/Documents/Docmation_Git_Projects/Base64-Java/spring-boot-file-upload-download-rest-api-example/uploadedFiles/Testfile12.jpg");
+        OutputStream os = new FileOutputStream(file);
+
+        // Starts writing the bytes in it
+        os.write(decodedBytes);
+        System.out.println("Successfully" + " byte inserted");
+
+        // Close the file
+        os.close();*/
+
+        /*FileUtils.writeByteArrayToFile(new File(outputFileName), decodedBytes);
+        String fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());*/
     }
 
     @PostMapping("/uploadMultipleFiles")
